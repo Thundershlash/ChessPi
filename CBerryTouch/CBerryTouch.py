734c1b0b543a1680d0c318ca8e1824593ea1908d
Thundershlash <thundershlash@gmx.net>
2017-01-28 18:17:43 +0100

    Bug fixes CBerryTouch.py

--- CBerryTouch/CBerryTouch.py
+++ CBerryTouch/CBerryTouch.py
@@ -35,196 +35,196 @@ class CBerryTouch():
    _PICTURE_PIXELS = _DISPLAY_WIDTH*_DISPLAY_HEIGHT
 
    # RAIO register -> see datasheet RAIO8870
-   _PCOD = 0x00
-   _PWRR = 0x01
-   _MRWC = 0x02
-   _PCLK = 0x04
+   _PCOD = '0x00'
+   _PWRR = '0x01'
+   _MRWC = '0x02'
+   _PCLK = '0x04'
 
-   _SYSR = 0x10
-   _DRGB = 0x11
-   _IOCR = 0x12
-   _IODR = 0x13
+   _SYSR = '0x10'
+   _DRGB = '0x11'
+   _IOCR = '0x12'
+   _IODR = '0x13'
 
-   _HDWR  = 0x14
-   _HNDFTR = 0x15
-   _HNDR  = 0x16
-   _HSTR  = 0x17
-   _HPWR  = 0x18
+   _HDWR  = '0x14'
+   _HNDFTR = '0x15'
+   _HNDR  = '0x16'
+   _HSTR  = '0x17'
+   _HPWR  = '0x18'
 
-   _VDHR0 = 0x19
-   _VDHR1 = 0x1a
-   _VNDR0 = 0x1b
-   _VNDR1 = 0x1c
-   _VSTR0 = 0x1d
-   _VSTR1 = 0x1e
-   _VPWR  = 0x1f
+   _VDHR0 = '0x19'
+   _VDHR1 = '0x1a'
+   _VNDR0 = '0x1b'
+   _VNDR1 = '0x1c'
+   _VSTR0 = '0x1d'
+   _VSTR1 = '0x1e'
+   _VPWR  = '0x1f'
 
-   _DPCR  = 0x20
-   _FNCR0 = 0x21
-   _FNCR1 = 0x22
-   _CGSR = 0x23
-   _HOFS0 = 0x24
-   _HOFS1 = 0x25
-   _VOFS0 = 0x26
-   _VOFS1 = 0x27
-   _ROMS = 0x28
+   _DPCR  = '0x20'
+   _FNCR0 = '0x21'
+   _FNCR1 = '0x22'
+   _CGSR  = '0x23'
+   _HOFS0 = '0x24'
+   _HOFS1 = '0x25'
+   _VOFS0 = '0x26'
+   _VOFS1 = '0x27'
+   _ROMS  = '0x28'
 
-   _FLDR = 0x29
+   _FLDR = '0x29'
 
-   _HSAW0 = 0x30
-   _HSAW1 = 0x31
-   _VSAW0 = 0x32
-   _VSAW1 = 0x33
-   _HEAW0 = 0x34
-   _HEAW1 = 0x35
-   _VEAW0 = 0x36
-   _VEAW1 = 0x37
-   _HSSW0 = 0x38
-   _HSSW1 = 0x39
-   _VSSW0 = 0x3a
-   _VSSW1 = 0x3b
-   _HESW0 = 0x3c
-   _HESW1 = 0x3d
-   _VESW0 = 0x3e
-   _VESW1 = 0x3f
+   _HSAW0 = '0x30'
+   _HSAW1 = '0x31'
+   _VSAW0 = '0x32'
+   _VSAW1 = '0x33'
+   _HEAW0 = '0x34'
+   _HEAW1 = '0x35'
+   _VEAW0 = '0x36'
+   _VEAW1 = '0x37'
+   _HSSW0 = '0x38'
+   _HSSW1 = '0x39'
+   _VSSW0 = '0x3a'
+   _VSSW1 = '0x3b'
+   _HESW0 = '0x3c'
+   _HESW1 = '0x3d'
+   _VESW0 = '0x3e'
+   _VESW1 = '0x3f'
 
-   _MWCR0 = 0x40
-   _MWCR1 = 0x41
-   _TFCR  = 0x42
-   _TBCR  = 0x43
-   _BTCR  = 0x44
-   _CURS  = 0x45
-   _CURH0 = 0x46
-   _CURH1 = 0x47
-   _CURV0 = 0x48
-   _CURV1 = 0x49
-   _RCURH0 = 0x4a
-   _RCURH01 = 0x4b
-   _RCURV0 = 0x4c
-   _RCURV1 = 0x4d
-   _MRCD  = 0x4e
-   _BECR0 = 0x50
-   _BECR1 = 0x51
-   _LTPR0 = 0x52
-   _LTPR1 = 0x53
-   _HSBE0 = 0x54
-   _HSBE1 = 0x55
-   _VSBE0 = 0x56
-   _VSBE1 = 0x57
-   _HDBE0 = 0x58
-   _HDBE1 = 0x59
-   _VDBE0 = 0x5a
-   _VDBE1 = 0x5b
-   _BEWR0 = 0x5c
-   _BEWR1 = 0x5d
-   _BEHR0 = 0x5e
-   _BEHR1 = 0x5f
+   _MWCR0 = '0x40'
+   _MWCR1 = '0x41'
+   _TFCR  = '0x42'
+   _TBCR  = '0x43'
+   _BTCR  = '0x44'
+   _CURS  = '0x45'
+   _CURH0 = '0x46'
+   _CURH1 = '0x47'
+   _CURV0 = '0x48'
+   _CURV1 = '0x49'
+   _RCURH0 = '0x4a'
+   _RCURH01 = '0x4b'
+   _RCURV0 = '0x4c'
+   _RCURV1 = '0x4d'
+   _MRCD  = '0x4e'
+   _BECR0 = '0x50'
+   _BECR1 = '0x51'
+   _LTPR0 = '0x52'
+   _LTPR1 = '0x53'
+   _HSBE0 = '0x54'
+   _HSBE1 = '0x55'
+   _VSBE0 = '0x56'
+   _VSBE1 = '0x57'
+   _HDBE0 = '0x58'
+   _HDBE1 = '0x59'
+   _VDBE0 = '0x5a'
+   _VDBE1 = '0x5b'
+   _BEWR0 = '0x5c'
+   _BEWR1 = '0x5d'
+   _BEHR0 = '0x5e'
+   _BEHR1 = '0x5f'
 
-   _BGCR0 = 0x60
-   _BGCR1 = 0x61
-   _BGCR2 = 0x62
-   _FGCR0 = 0x63
-   _FGCR1 = 0x64
-   _FGCR2 = 0x65
-   _PTNO  = 0x66
-   _BGTR  = 0x67
+   _BGCR0 = '0x60'
+   _BGCR1 = '0x61'
+   _BGCR2 = '0x62'
+   _FGCR0 = '0x63'
+   _FGCR1 = '0x64'
+   _FGCR2 = '0x65'
+   _PTNO  = '0x66'
+   _BGTR  = '0x67'
 
-   _TPCR0 = 0x70
-   _TPCR1 = 0x71
-   _TPXH  = 0x72
-   _TPYH  = 0x73
-   _TPXYL = 0x74
+   _TPCR0 = '0x70'
+   _TPCR1 = '0x71'
+   _TPXH  = '0x72'
+   _TPYH  = '0x73'
+   _TPXYL = '0x74'
 
-   _GCHP0 = 0x80
-   _GCHP1 = 0x81
-   _GCVP0 = 0x82
-   _GCVP1 = 0x83
-   _GCC0  = 0x84
-   _GCC1  = 0x85
+   _GCHP0 = '0x80'
+   _GCHP1 = '0x81'
+   _GCVP0 = '0x82'
+   _GCVP1 = '0x83'
+   _GCC0  = '0x84'
+   _GCC1  = '0x85'
 
-   _PLLC1 = 0x88
-   _PLLC2 = 0x89
+   _PLLC1 = '0x88'
+   _PLLC2 = '0x89'
 
-   _P1CR  = 0x8a
-   _P1DCR = 0x8b
-   _P2CR  = 0x8c
-   _P2DCR = 0x8d
-   _MCLR  = 0x8e
-   _INTC  = 0x8f
+   _P1CR  = '0x8a'
+   _P1DCR = '0x8b'
+   _P2CR  = '0x8c'
+   _P2DCR = '0x8d'
+   _MCLR  = '0x8e'
+   _INTC  = '0x8f'
 
-   _DCR   = 0x90
-   _DLHSR0 = 0x91
-   _DLHSR1 = 0x92
-   _DLVSR0 = 0x93
-   _DLVSR1 = 0x94
-   _DLHER0 = 0x95
-   _DLHER1 = 0x96
-   _DLVER0 = 0x97
-   _DLVER1 = 0x98
-   _DCHR0  = 0x99
-   _DCHR1 = 0x9a
-   _DCVR0 = 0x9b
-   _DCVR1 = 0x9c
-   _DCRR  = 0x9d
+   _DCR    = '0x90'
+   _DLHSR0 = '0x91'
+   _DLHSR1 = '0x92'
+   _DLVSR0 = '0x93'
+   _DLVSR1 = '0x94'
+   _DLHER0 = '0x95'
+   _DLHER1 = '0x96'
+   _DLVER0 = '0x97'
+   _DLVER1 = '0x98'
+   _DCHR0  = '0x99'
+   _DCHR1  = '0x9a'
+   _DCVR0  = '0x9b'
+   _DCVR1  = '0x9c'
+   _DCRR   = '0x9d'
 
-   _TCR1 = 0xa0
-   _TCR2 = 0xa1
-   _OEHTCR1 = 0xa2
-   _OEHTCR2 = 0xa3
-   _OEHTCR3 = 0xa4
-   _OEHTCR4 = 0xa5
-   _OEHTCR5 = 0xa6
-   _OEHTCR6 = 0xa7
-   _OEHTCR7 = 0xa8
-   _OEHTCR8 = 0xa9
+   _TCR1 = '0xa0'
+   _TCR2 = '0xa1'
+   _OEHTCR1 = '0xa2'
+   _OEHTCR2 = '0xa3'
+   _OEHTCR3 = '0xa4'
+   _OEHTCR4 = '0xa5'
+   _OEHTCR5 = '0xa6'
+   _OEHTCR6 = '0xa7'
+   _OEHTCR7 = '0xa8'
+   _OEHTCR8 = '0xa9'
 
-   _STHTCR1 = 0xaa
-   _STHTCR2 = 0xab
-   _STHTCR3 = 0xac
-   _STHTCR4 = 0xad
+   _STHTCR1 = '0xaa'
+   _STHTCR2 = '0xab'
+   _STHTCR3 = '0xac'
+   _STHTCR4 = '0xad'
 
-   _Q1HCR1 = 0xae
-   _Q1HCR2 = 0xaf
+   _Q1HCR1 = '0xae'
+   _Q1HCR2 = '0xaf'
 
-   _OEVTCR1 = 0xb0
-   _OEVTCR2 = 0xb1
-   _OEVTCR3 = 0xb2
-   _OEVTCR4 = 0xb3
-   _CKVTCR1 = 0xb4
-   _CKVTCR2 = 0xb5
-   _CKVTCR3 = 0xb6
-   _CKVTCR4 = 0xb7
-   _STVTCR1 = 0xb8
-   _STVTCR2 = 0xb9
-   _STVTCR3 = 0xba
-   _STVTCR4 = 0xbb
-   _STVTCR5 = 0xbc
-   _STVTCR6 = 0xbd
-   _STVTCR7 = 0xbe
-   _STVTCR8 = 0xbf
+   _OEVTCR1 = '0xb0'
+   _OEVTCR2 = '0xb1'
+   _OEVTCR3 = '0xb2'
+   _OEVTCR4 = '0xb3'
+   _CKVTCR1 = '0xb4'
+   _CKVTCR2 = '0xb5'
+   _CKVTCR3 = '0xb6'
+   _CKVTCR4 = '0xb7'
+   _STVTCR1 = '0xb8'
+   _STVTCR2 = '0xb9'
+   _STVTCR3 = '0xba'
+   _STVTCR4 = '0xbb'
+   _STVTCR5 = '0xbc'
+   _STVTCR6 = '0xbd'
+   _STVTCR7 = '0xbe'
+   _STVTCR8 = '0xbf'
 
-   _COMTCR1 = 0xc0
-   _COMTCR2 = 0xc1
-   _RGBTCR1 = 0xc2
-   _RGBTCR2 = 0xc3
+   _COMTCR1 = '0xc0'
+   _COMTCR2 = '0xc1'
+   _RGBTCR1 = '0xc2'
+   _RGBTCR2 = '0xc3'
 
    # colors "RRRGGGBB"
-   _COLOR_RED     =  0xE0
-   _COLOR_BLUE    =  0x03
-   _COLOR_GREEN   =  0x1C
-   _COLOR_BLACK   =  0x00
-   _COLOR_WHITE   =  0xFF
-   _COLOR_CYAN    =  0x1F 
-   _COLOR_YELLOW  =  0xFC
-   _COLOR_MAGENTA =  0xE3
-   _COLOR_DARK_GREEN = 0x0C
+   _COLOR_RED     =  '0xE0'
+   _COLOR_BLUE    =  '0x03'
+   _COLOR_GREEN   =  '0x1C'
+   _COLOR_BLACK   =  '0x00'
+   _COLOR_WHITE   =  '0xFF'
+   _COLOR_CYAN    =  '0x1F'
+   _COLOR_YELLOW  =  '0xFC'
+   _COLOR_MAGENTA =  '0xE3'
+   _COLOR_DARK_GREEN = '0x0C'
 
    # ROP functions
-   _ROP_SOURCE = 0xC
+   _ROP_SOURCE = '0xC'
 
    # BTE operation functions
-   _BTE_MOVE_POSITIVE =  0x02
-   _BTE_SOLID_FILL   =   0x0C
+   _BTE_MOVE_POSITIVE =  '0x02'
+   _BTE_SOLID_FILL    =  '0x0C'
 
    # enumeration of drawing modes
    _DRAW_MODES = {'CIRCLE_NONFILL','CIRCLE_FILL','SQUARE_NONFILL','SQUARE_FILL','LINE'}
@@ -381,13 +381,13 @@ class CBerryTouch():
       if PLL_Initial_Flag == 0:           # wait until PLL is ready
          PLL_Initial_Flag = 1                # set Flag to avoid repeated PLL init
       
-         self.RAIO_SetRegister( self._PLLC1, 0x07 )     # set sys_clk 
+         self.RAIO_SetRegister( self._PLLC1, '0x07' )     # set sys_clk 
          bcm2835_DelayMicroseconds( 200 )
-         self.RAIO_SetRegister( self._PLLC2, 0x03 )     # set sys_clk 
+         self.RAIO_SetRegister( self._PLLC2, '0x03' )     # set sys_clk 
          bcm2835_delayMicroseconds( 200 )
       
-         self.RAIO_SetRegister( self._PWRR, 0x01 )     # Raio software reset ( bit 0 ) set
-         self.RAIO_SetRegister( self._PWRR, 0x00 )     # Raio software reset ( bit 0 ) set to 0
+         self.RAIO_SetRegister( self._PWRR, '0x01' )     # Raio software reset ( bit 0 ) set
+         self.RAIO_SetRegister( self._PWRR, '0x00' )     # Raio software reset ( bit 0 ) set to 0
          time.sleep(.100) 
 
 
@@ -395,22 +395,22 @@ class CBerryTouch():
    
          if self._COLOR_MODE == 'CM_65K': 
          # System Configuration Register
-            self.RAIO_SetRegister( self._SYSR, 0x0A )    # digital TFT
+            self.RAIO_SetRegister( self._SYSR, '0x0A' )    # digital TFT
                                             # parallel data out
                                             # no external memory
                                             # 8bit memory data bus
                                             # 16bpp 65K color
                                             # 16bit MCU-interface (data)
-            self.RAIO_SetRegister( self._DPCR, 0x00 )    # one layer   
+            self.RAIO_SetRegister( self._DPCR, '0x00' )    # one layer   
          elif _COLOR_MODE == 'CM_4K':
             # System Configuration Register
-            self.RAIO_SetRegister( self._SYSR, 0x06 )    # digital TFT
+            self.RAIO_SetRegister( self._SYSR, '0x06' )    # digital TFT
                                             # parallel data out
                                             # no external memory
                                             # 8bit memory data bus
                                             # 12bpp 4K color
                                             # 16bit MCU-interface (data)
-            self.RAIO_SetRegister( self._DPCR, 0x80 )    # two layers   
+            self.RAIO_SetRegister( self._DPCR, '0x80' )    # two layers   
             self.RAIO_SetRegister( self._MWCR1, self.BankNo_WR )
             self.RAIO_SetRegister( self._LTPR0, self.BankNo_RD )                      
  
@@ -418,19 +418,19 @@ class CBerryTouch():
       # 0x27+1 * 8 = 320 pixel  
       self.RAIO_SetRegister( self._HDWR , (self._DISPLAY_WIDTH / 8) - 1 )
       # Horizontal Non-Display Period Fine Tuning   
-      self.RAIO_SetRegister( self._HNDFTR, 0x02 )
+      self.RAIO_SetRegister( self._HNDFTR, '0x02' )
     
       # HNDR , Horizontal Non-Display Period Bit[4:0] 
       # Horizontal Non-Display Period (pixels) = (HNDR + 1)*8    
-      self.RAIO_SetRegister( self._HNDR, 0x03 )
+      self.RAIO_SetRegister( self._HNDR, '0x03' )
       # HSTR , HSYNC Start Position[4:0], HSYNC Start Position(PCLK) = (HSTR + 1)*8     0x02    
-      self.RAIO_SetRegister( self._HSTR, 0x04 )                                 
+      self.RAIO_SetRegister( self._HSTR, '0x04' )                                 
 
       # HPWR , HSYNC Polarity ,The period width of HSYNC. 
       # 1xxxxxxx activ high 0xxxxxxx activ low
       # HSYNC Width [4:0] HSYNC Pulse width
       # (PCLK) = (HPWR + 1)*8
-      self.RAIO_SetRegister( self._HPWR, 0x03 )
+      self.RAIO_SetRegister( self._HPWR, '0x03' )
     
     
       # vertical settings    
